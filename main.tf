a07a5bfc76060bf9ea69e4509a81435c9a71e1f6
adrianAtDvelop <63459798+adrianAtDvelop@users.noreply.github.com>
2020-07-09 16:05:44 +0200

    Added elastic beanstalk reference

--- main.tf
+++ main.tf
@@ -19,3 +19,15 @@ resource "aws_instance" "example" {
   ami           = "ami-00224432b596f1c45"
   instance_type = "t2.micro"
        }
+
+resource "aws_elastic_beanstalk_application" "test" {
+    name = "testapp"
+    description = "tf-test-desc"
+}
+
+resource "aws_elastic_beanstalk_environment" "testenv" {
+    name = "testenv"
+    application = "aws_elastic_beanstalk_application.testapp.name"
+    solution_stack_name = "64bit Windows Server Core 2019 v2.5.7 running IIS 10.0"
+
+}
