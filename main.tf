e40a95c541f32308481d205c4272cc6b85a571ba
Adrian Steinbach <aste@d-velop.de>
2020-07-13 12:27:13 +0200

    Implementation of ebs deployment config

--- main.tf
+++ main.tf
@@ -19,3 +19,14 @@ resource "aws_instance" "example" {
   ami           = "ami-00224432b596f1c45"
   instance_type = "t2.micro"
        }
+
+resource "aws_elastic_beanstalk_application" "tftest" {
+  name        = "tf-test-name"
+  description = "tf-test-desc"
+}
+
+resource "aws_elastic_beanstalk_environment" "tfenvtest" {
+  name                = "tf-test-name"
+  application         = "${aws_elastic_beanstalk_application.tftest.name}"
+  solution_stack_name = "64bit Windows Server Core 2019 v2.5.7 running IIS 10.0"
+}
